e82e50654bf991e73c09b790e95bcdc9e094ee32
4escofet <agustin.escofet@gmail.com>
2022-07-31 22:35:34 -0300

    first app update
    content update

--- SVELTE/npx-projects/my-firstapp/src/App.svelte
+++ SVELTE/npx-projects/my-firstapp/src/App.svelte
@@ -1,12 +1,22 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+  import Numeros from './lib/Numeros.svelte'
+  
+  const cursos = ['html', 'css', 'js']
+
 </script>
-
-<main>
-  <div>
-
-
+<main class='container'>
+<!-- Lista de cursos-->
+  <div> 
+<h1> Cursos</h1>
+<ul>
+  {#each cursos as item, index}
+     <li> {index + 1} - {item} </li>
+  {/each}
+</ul>
+  </div>
+  <Numeros/>
   <div class="card">
     <Counter />
   </div>
---
+++ SVELTE/npx-projects/my-firstapp/src/lib/Numeros.svelte
@@ -0,0 +1,27 @@
+ <!-- Lista de numeros-->
+ <script>
+ // Lista de cursos y números
+
+  let numeros = [ 1,5,4]
+  const agregarNumero = () => {
+    let num = Math.floor(Math.random()*(100 - 1)) + 1;
+    console.log(num);
+    numeros = [...numeros,num];
+  };
+
+ </script>
+ 
+ <div>
+    
+    <ul>
+      {#each numeros as item}
+         <li>{item} </li>
+      {/each}
+    
+      {#if numeros.length>5}
+         <h3> Se agregaron 5 números</h3>
+      {:else}
+      <button on:click={agregarNumero} class= "btn btn-primary"> Agregar número </button>
+      {/if}
+    </ul> 
+</div>
